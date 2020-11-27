58bdb223e9db33479e769c1aaf11a736b380118d
Ken <kenny2minecraft@gmail.com>
2020-11-28 00:59:07 +0800

    Implement new DB layout. (#14)
    * Implement new DB layout. Closes #2 again
    
    * whoops

--- backend/sql/v0.sql
+++ backend/sql/v0.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS scratch_users (
   -- Scratch user ID
   user_id integer PRIMARY KEY,
   -- Scratch username
-  user_name text UNIQUE NOT NULL
+  user_name text UNIQUE NOT NULL,
+  -- arbitrary data in case we need it
+  data text
 );
 
 CREATE TABLE IF NOT EXISTS applications (
@@ -18,34 +20,36 @@ CREATE TABLE IF NOT EXISTS applications (
   app_name text,
   -- Scratch user ID of owner
   owner_id integer NOT NULL,
-  -- owner
-  owner_name text NOT NULL,
   -- app names must be approved
-  name_approved boolean DEFAULT FALSE,
+  approved boolean DEFAULT FALSE,
   -- FK
   FOREIGN KEY(owner_id) REFERENCES scratch_users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS approvals (
+  -- refresh token to get new access token
+  refresh_token text PRIMARY KEY,
   -- Scratch user ID of approver
   user_id integer NOT NULL,
-  -- username as well
-  user_name text NOT NULL,
   -- client ID being approved
   client_id integer NOT NULL,
+  -- access token currently in use, nullable
+  access_token text,
   -- stringified list of scopes
-  scopes text,
-  -- keys
-  PRIMARY KEY(user_id, client_id),
+  scopes text NOT NULL,
+  -- approvals don't last forever, only until this time
+  expiry integer NOT NULL,
+  -- FKs
   FOREIGN KEY(user_id) REFERENCES scratch_users(user_id),
-  FOREIGN KEY(client_id) REFERENCES applications(client_id)
+  FOREIGN KEY(client_id) REFERENCES applications(client_id),
+  FOREIGN KEY(access_token) REFERENCES authings(code)
 );
 
 CREATE TABLE IF NOT EXISTS redirect_uris (
-  -- application ID this redirect URI is for
-  client_id integer PRIMARY KEY,
   -- URI in question
-  redirect_uri text NOT NULL,
+  redirect_uri text PRIMARY KEY,
+  -- application ID this redirect URI is for
+  client_id integer NOT NULL,
   -- FK
   FOREIGN KEY(client_id) REFERENCES applications(client_id)
 );
@@ -56,24 +60,29 @@ DROP TABLE IF EXISTS sessions;
 CREATE TABLE sessions (
   session_id integer PRIMARY KEY,
   -- Scratch user ID of session owner
-  user_id integer NOT NULL,
+  user_id integer,
   -- expiry time: unix epoch integer
-  expiry integer,
+  expiry integer NOT NULL,
   -- reference to auth process data, if in the midst
   authing text,
+  -- nonce used for login
+  nonce text,
   -- keys
   FOREIGN KEY(user_id) REFERENCES scratch_users(user_id),
-  FOREIGN KEY(authing) REFERENCES authings(codetoken)
+  FOREIGN KEY(authing) REFERENCES authings(code)
 );
 
 CREATE TABLE IF NOT EXISTS authings (
-  -- token-get code, or refresh token
-  codetoken text PRIMARY KEY,
-  -- the following are all data used during auth
+  -- token-get code, or access token
+  code text PRIMARY KEY,
+  -- data associated with this access token
+  -- or if is token-get code, data associated
+  -- with this to-be-access token
   client_id integer NOT NULL,
   redirect_uri text NOT NULL,
   scopes text NOT NULL,
   state text,
+  expiry integer,
   -- FK
   FOREIGN KEY(client_id) REFERENCES applications(client_id)
 );
