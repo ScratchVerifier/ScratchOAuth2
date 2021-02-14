82b8ad66c9c62dafe01830cc8d63f0f396eac2fa
Kenny2github <kenny2minecraft@gmail.com>
2021-02-14 19:57:55 +0000

    copy some constants from previous iteration

---
+++ includes/common/consts.php
@@ -0,0 +1,5 @@
+<?php
+
+define('SOA2_COMMENTS_API', 'https://scratch.mit.edu/site-api/comments/user/%s?page=1&salt=%s');
+define('SOA2_USERS_API', 'https://api.scratch.mit.edu/users/%s');
+define('SOA2_USERNAME_REGEX', '%^[A-Za-z0-9_-]{3,20}$%');
