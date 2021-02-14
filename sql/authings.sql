4f5d9cc80b170c68b029ec16a1e2c74d0953c796
Kenny2github <kenny2minecraft@gmail.com>
2021-02-14 19:45:43 +0000

    Implement MW DB layout (closes #35)
    + Extension schema update registration and hooks class
    + SQL files for each table

---
+++ sql/authings.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS /*_*/soa2_authings (
+	-- the token-getter code
+	code char(64) binary NOT NULL,
+	-- who is getting the token
+	client_id integer unsigned NOT NULL,
+	-- who is giving the token
+	user_id integer unsigned NOT NULL,
+	-- arbitrary string
+	state varchar(255) binary NOT NULL,
+	-- URI to redirect to (can be null)
+	redirect_uri varchar(1024) binary,
+	-- space-separated list of scopes being requested
+	scopes varchar(255) binary NOT NULL,
+	-- unix timestamp when this request expires
+	expiry integer unsigned NOT NULL,
+	-- keys
+	PRIMARY KEY(code),
+	FOREIGN KEY(client_id) REFERENCES /*_*/soa2_applications(client_id),
+	FOREIGN KEY(user_id) REFERENCES /*_*/soa2_scratchers(user_id)
+) /*$wgDBTableOptions*/;
+
+COMMIT;
---
+++ sql/redirect_uris.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS /*_*/soa2_redirect_uris (
+	-- the URI
+	redirect_uri varchar(1024) binary NOT NULL,
+	-- which app's URI?
+	client_id integer unsigned NOT NULL,
+	-- keys
+	PRIMARY KEY(redirect_uri),
+	FOREIGN KEY(client_id) REFERENCES /*_*/soa2_applications(client_id)
+) /*$wgDBTableOptions*/;
+
+COMMIT;
