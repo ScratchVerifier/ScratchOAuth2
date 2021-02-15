1fda791e87a8378e43e166862bb264888052d58d
Kenny2github <kenny2minecraft@gmail.com>
2021-02-15 22:12:59 +0000

    Implement login submitting (closes #38)
    includes/:
    
    common/consts.php:
    + Comments regex, copied from previous implementation
    
    +common/db.php:
    * Database methods, currently just user methods
    
    common/login.php:
    * Namespace
    * Consolidate code generation into a method
    * Change of plan: code is generated entirely randomly,
      and set in the session until used
    * Consolidate database mechanics into new db.php
    * Clarify where username needs to be case insensitive
    + Method to check for code on profile
    + Implement login submitting - returns success bool
    
    api/Login.php:
    * Fix: use SOA2Login
    + POST implementation
    
    special/SpecialScratchOAuth2.php:
    * SOA2Login is namespaced now
    * Default case shows Scratch user ID from session, for debugging
    * Change username and token checks to presence checks,
      rather than non-emptiness checks
    * Move username format check outside of POST handlers
    * Allow prefilling username input
    + Submit implementation
    
    i18n/*.json:
    + soa2-login-*: see qqq.json

--- includes/common/consts.php
+++ includes/common/consts.php
@@ -4,5 +4,6 @@ define('SOA2_COMMENTS_API', 'https://scratch.mit.edu/site-api/comments/user/%s?p
 define('SOA2_USERS_API', 'https://api.scratch.mit.edu/users/%s');
 define('SOA2_PROFILE_URL', 'https://scratch.mit.edu/users/%s');
 define('SOA2_USERNAME_REGEX', '%^[A-Za-z0-9_-]{3,20}$%');
+define('SOA2_COMMENTS_REGEX', '%<div id="comments-\d+" class="comment +" data-comment-id="\d+">.*?<div class="actions-wrap">.*?<div class="name">\s+<a href="/users/([_a-zA-Z0-9-]+)">\1</a>\s+</div>\s+<div class="content">\s*(.*?)\s*</div>%s');
 
 define('SOA2_CODE_EXPIRY', 30*60);
--- includes/common/login.php
+++ includes/common/login.php
@@ -1,6 +1,23 @@
 <?php
 
+namespace MediaWiki\Extension\ScratchOAuth2\Common;
+
+require_once __DIR__ . "/consts.php";
+require_once __DIR__ . "/db.php";
+
 class SOA2Login {
+	/**
+	 * Generate the Scratch verification code.
+	 * @param string $username the username to generate the code for
+	 */
+	public static function gen_code( $session ) {
+		if (!$session->exists( 'soa2_scratch_code' )) {
+			$code = strtr(hash('sha256', random_bytes(32)), '0123456789', 'ABCDEFGHIJ');
+			$session->set( 'soa2_scratch_code', $code );
+			$session->save();
+		}
+		return $session->get( 'soa2_scratch_code' );
+	}
 	/**
 	 * Get the data needed to complete a login.
 	 * @param string $username the username to get the codes for
@@ -13,23 +30,51 @@ class SOA2Login {
 		if (!$user) return null;
 		// save user data
 		$username = $user['username'];
-		$db = wfGetDB( DB_MASTER );
-		$values = [
-			'user_id' => $user['id'],
-			'user_name' => $username
-		];
-		$db->upsert('soa2_scratchers', $values, array_keys($values), $values);
+		SOA2DB::saveUser( $user['id'], $user['username'] );
 		// actually do the code generation
 		$session = $wgRequest->getSession();
 		$session->persist();
 		$csrf = $session->getToken()->toString();
 		// Step 15
-		$code = hash(
-			'sha256',
-			hash('sha256', $csrf)
-			. hash('sha256', $username)
-			. hash('sha256', '' . floor(time() / SOA2_CODE_EXPIRY))
-		);
+		$code = self::gen_code( $session ); // Step 15
 		return [ 'username' => $username, 'csrf' => $csrf, 'code' => $code ];
+	}
+	/**
+	 * Did the user comment this code?
+	 * @param string $code the code
+	 * @param string $username the user
+	 */
+	public static function commented( string $username, string $code ) {
+		$username = strtolower($username);
+		// Step 20, 21
+		$comments = file_get_contents(sprintf(SOA2_COMMENTS_API, $username, rand()));
+		$matches = [];
+		preg_match_all(SOA2_COMMENTS_REGEX, $comments, $matches, PREG_PATTERN_ORDER);
+		for ($i = 0; $i < count($matches[0]); ++$i) {
+			if (strtolower($matches[1][$i]) != $username) continue;
+			if (hash_equals($code, $matches[2][$i])) return true; // Step 22
+		}
+		return false;
+	}
+	/**
+	 * Log in a user
+	 * @param string $username the username to login
+	 * @param string $csrf the CSRF token submitted
+	 * @return boolean whether the login was successful
+	 */
+	public static function login( string $username, string $csrf ) {
+		global $wgRequest;
+		$session = $wgRequest->getSession();
+		$session->persist();
+		$token = $session->getToken();
+		if (!$session->getToken()->match($csrf, SOA2_CODE_EXPIRY)) return false;
+		$code = self::gen_code( $session ); // Step 19
+		if (!self::commented( $username, $code )) return false;
+		// the login's successful, set the user ID
+		$user = SOA2DB::getUserByName( $username );
+		$session->set( 'soa2_user_id', $user->user_id ); // Step 23
+		$session->remove( 'soa2_scratch_code' );
+		$session->save();
+		return true;
 	}
 }
