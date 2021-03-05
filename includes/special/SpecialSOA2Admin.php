bfd8cff0d1edfd72f0998fd8f4f3ee851a2b163f
Kenny2github <kenny2minecraft@gmail.com>
2021-03-05 15:00:06 +0000

    Add admin view of app
    extension.json:
    + Register Special:SOA2Admin
    
    i18n/*.json:
    * See qqq
    
    includes/common/apps.php:
    * Update the phpdoc to note the nullable owner ID
    * Include the owner_id field if owner ID passed is null
    
    +includes/special/SpecialSOA2Admin.php:
    + app() shows data for an app that an admin would need to see,
      and also provides mechanisms to change it. Note: client
      secret is not shown, but there is a checkbox to reset it, so
      that the admin theoretically can't use someone else's app
      but can reset the secret if necessary for some reason;
      app name is not modifiable, instead the admin must tell the
      user to change it themselves if it can't be approved;
      flags are not only displayed, but also modifiable. However,
      the POST action is not yet implemented.
    * Redirect to login if not logged in; show error if not admin

---
+++ includes/special/SpecialSOA2Admin.php
@@ -0,0 +1,147 @@
+<?php
+namespace MediaWiki\Extension\ScratchOAuth2\Special;
+
+require_once dirname(__DIR__) . "/common/consts.php";
+require_once dirname(__DIR__) . "/common/apps.php";
+require_once dirname(__DIR__) . "/common/users.php";
+
+use SpecialPage;
+use Html;
+use ReflectionClass;
+use MediaWiki\Extension\ScratchOAuth2\Common\AppFlags;
+use MediaWiki\Extension\ScratchOAuth2\Common\SOA2Apps;
+use MediaWiki\Extension\ScratchOAuth2\Common\SOA2Users;
+
+class SpecialSOA2Admin extends SpecialPage {
+	public function __construct() {
+		parent::__construct( 'SOA2Admin' );
+	}
+
+	public function execute( $par ) {
+		$this->checkReadOnly();
+		$out = $this->getOutput();
+		$out->setIndexPolicy( 'noindex' );
+		$user_id = intval(SOA2Apps::userID());
+		if (!$user_id) {
+			$out->setPageTitle( wfMessage('notloggedin')->escaped() );
+			$out->redirect(
+				$this->getTitleFor( 'ScratchOAuth2', 'login' )->getFullURL([
+					'returnto' => $this->getPageTitle( $par )->getLinkURL()
+				]), 303
+			);
+			return;
+		}
+		// temp until config
+		if ($user_id != 10114764 && $user_id != 35751470 && $user_id != 62962013) {
+			$out->showErrorPage('soa2-not-admin-title', 'soa2-not-admin');
+			return;
+		}
+		$path = explode('/', $par);
+		$par = $path[0];
+		$path = array_slice($path, 1);
+		switch ( $par ) {
+			case 'approvals':
+				$this->approvals( $path );
+				break;
+			case 'app':
+				$this->app( $path );
+				break;
+			default:
+				$out->setPageTitle( 'SOA2Admin' );
+				$out->addHTML(Html::element(
+					'pre', [],
+					var_export($this->getRequest()->getIntArray('test'), true)
+				));
+				$out->addHTML(Html::element('pre', [], var_export($user_id, true)));
+		}
+	}
+	public function app( array $path ) {
+		$out = $this->getOutput();
+		if (
+			count($path) < 1
+			|| !ctype_digit($path[0])
+			|| !($app = SOA2Apps::application( intval($path[0]), null ))
+		) {
+			$out->showErrorPage('soa2-no-app-title', 'soa2-no-app', $path);
+			return;
+		}
+		if ($this->getRequest()->wasPosted()) {
+			$app = $this->saveApp( $path, $app );
+		}
+		$out->setPageTitle(
+			htmlspecialchars($app['app_name'])
+			?: wfMessage('soa2-unnamed-app')->escaped()
+		);
+		$out->addReturnTo($this->getPageTitle());
+		$out->addHTML(Html::openElement('form', [ 'method' => 'POST' ]));
+		$out->addHTML(Html::hidden('token',
+			$this->getRequest()->getSession()->getToken()->toString()));
+		$out->addHTML(Html::openElement('table', [ 'class' => 'wikitable' ]));
+		$out->addHTML(Html::openElement('tr'));
+			$out->addHTML(Html::element('th', [], wfMessage('soa2-app-id')->text()));
+			$out->addHTML(Html::rawElement('td', [], Html::element(
+				'code', [], (string)$app['client_id']
+			)));
+		$out->addHTML(Html::closeElement('tr'));
+		$out->addHTML(Html::openElement('tr'));
+			$out->addHTML(Html::element(
+				'th', [],
+				wfMessage('soa2-app-name')->text(),
+			));
+			$out->addHTML(Html::element(
+				'td', [],
+				$app['app_name']
+			));
+		$out->addHTML(Html::closeElement('tr'));
+		$out->addHTML(Html::openElement('tr'));
+			$out->addHTML(Html::element(
+				'th', [],
+				wfMessage('soa2-app-owner')->text()
+			));
+			$username = SOA2Users::getName($app['owner_id']);
+			$out->addHTML(Html::rawElement('td', [], Html::element(
+				'a',
+				[
+					'href' => sprintf(SOA2_PROFILE_URL, $username),
+					'target' => '_new'
+				],
+				$username
+			)));
+		$out->addHTML(Html::closeElement('tr'));
+		$out->addHTML(Html::closeElement('table'));
+		$out->addHTML(Html::rawElement('p', [], Html::check(
+			'reset_secret', false, [ 'id' => 'soa2-reset-secret-input' ]
+		) . Html::label(
+			wfMessage('soa2-app-reset')->escaped(),
+			'soa2-reset-secret-input'
+		)));
+		// flags
+		$out->addHTML(Html::openElement('table', [ 'class' => 'wikitable' ]));
+		$out->addHTML(Html::element('caption', [], wfMessage('soa2-flags')->text()));
+		$flagsClass = new ReflectionClass(AppFlags::class);
+		foreach ($flagsClass->getConstants() as $name => $value) {
+			$out->addHTML(Html::openElement('tr'));
+			$out->addHTML(Html::rawElement('th', [], Html::label(
+				wfMessage("soa2-flags-$name")->text(),
+				"soa2-flag-$name-input",
+				[ 'title' => $name ],
+			)));
+			$out->addHTML(Html::rawElement('td', [], Html::check(
+				'flags[]', (bool)($app['flags'] & $value),
+				[ 'value' => $value, 'id' => "soa2-flag-$name-input" ]
+			)));
+		}
+		$out->addHTML(Html::closeElement('table'));
+		$out->addHTML(Html::textarea(
+			'redirect_uris',
+			implode("\n", $app['redirect_uris']),
+			[ 'id' => 'soa2-redirect-uris-input']
+		));
+		$out->addHTML(Html::rawElement('p', [], Html::input(
+			'save',
+			wfMessage('soa2-app-save')->text(),
+			'submit'
+		)));
+		$out->addHTML(Html::closeElement('form'));
+	}
+}
